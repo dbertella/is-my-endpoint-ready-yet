01c228e22a83b8014147bd9790bdadf93625bdef
dbertella <daniele.bertella@gmail.com>
2018-11-16 14:35:52 +0000

    Updates

---
+++ precache-manifest.97b13116684309d576c7f0256223f580.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "603c7985e4467672bb20",
+    "url": "/is-my-endpoint-ready-yet/static/css/main.f0705a8b.chunk.css"
+  },
+  {
+    "revision": "603c7985e4467672bb20",
+    "url": "/is-my-endpoint-ready-yet/static/js/main.603c7985.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/is-my-endpoint-ready-yet/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "213398e98dd802863694",
+    "url": "/is-my-endpoint-ready-yet/static/js/runtime~main.213398e9.js"
+  },
+  {
+    "revision": "00f3bb9aa7d77f5779ec4e54f96f7269",
+    "url": "/is-my-endpoint-ready-yet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d0617393d5ea6877a77db80528c0903.js"
+  "/is-my-endpoint-ready-yet/precache-manifest.97b13116684309d576c7f0256223f580.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/is-my-endpoint-ready-yet/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
