38b14901657db2f987b027b430a291df1d77a7f2
dbertella <daniele.bertella@gmail.com>
2018-11-16 14:31:02 +0000

    Add content

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo">🤔</div>
+          <p>
+            Is my endpoint ready yet?
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
