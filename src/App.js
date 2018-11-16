15548b04e2de791c461efde8adc5f76aa0debbdb
dbertella <daniele.bertella@gmail.com>
2018-11-16 15:00:41 +0000

    Add yes and no

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  state = {
+    yes: false,
+    no: false,
+  }
+
+  yes = () => {
+    this.setState({
+      yes: true,
+      no: false,
+    })
+  }
+
+  no = () => {
+    this.setState({
+      no: true,
+      yes: false,
+    })
+  }
+
   render() {
+    const { yes, no } = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <div className="App-logo">🤔</div>
-          <p>
-            Is my endpoint ready yet?
-          </p>
+          <span className="App-logo" role="img" aria-label="thinking">
+            {!(yes || no) && '🤔'}
+            {yes && '👏'}
+            {no && '👎'}
+          </span>
+          <p>Is my endpoint ready yet?</p>
+          <div>
+            <button onClick={this.yes}>Yes</button>
+            <button onClick={this.no}>No</button>
+          </div>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
